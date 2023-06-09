c05168d5c1ec81ee4f1d0378d3bea98c24a65f82
Schlomoh <post@moritzbecker.de>
2023-06-09 18:57:24 +0200

    add cookie consent banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { CookieBanner } from "@schlomoh/react-cookieconsent";
 import {
   Footer,
   NavBar,
@@ -5,6 +6,35 @@ import {
   ThemeProvider,
 } from "@/components";
 import { Home } from "./pages";
+import { CSSProperties, useMemo } from "react";
+import { useTheme } from "styled-components";
+
+const CustomCookieBanner = () => {
+  const { colors } = useTheme();
+
+  const containerStyle = useMemo(
+    () =>
+      ({
+        backgroundColor: colors.base.white,
+        boxSizing: "border-box",
+      } as CSSProperties),
+    []
+  );
+  const buttonStyle = useMemo(
+    () =>
+      ({
+        backgroundColor: colors.accent.blue,
+        boxSizing: "border-box",
+      } as CSSProperties),
+    []
+  );
+  return (
+    <CookieBanner
+      containerStyle={containerStyle}
+      primaryButtonStyle={buttonStyle}
+    />
+  );
+};
 
 function App() {
   return (
@@ -13,6 +43,7 @@ function App() {
         <NavBar />
         <Home />
         <Footer />
+        <CustomCookieBanner />
       </ThemeProvider>
     </NavBarContextProvider>
   );
